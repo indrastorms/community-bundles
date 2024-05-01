a5be092b1e5c21a40bb6b165e80dcf715d2a47f4
Indranil012 <indranilchowdhury0@gmail.com>
2024-05-01 12:00:25 +0530

    using async

--- gen_bundle.py
+++ gen_bundle.py
@@ -1,42 +1,45 @@
-import requests
+import asyncio
 import json
+from httpx import AsyncClient
 
-def get_latest_release(repo_url, prerelease = False):
-    def get_version_url(release):
+async def get_latest_release(repo_url, prerelease = False):
+    async def get_version_url(release):
         version = release['tag_name']
         for asset in release["assets"]:
             if asset["browser_download_url"].endswith(".jar")  or\
                asset["browser_download_url"].endswith(".apk"):
                 asset_url = asset['browser_download_url']
                 return version, asset_url
+        print(f"No asset found for the {version}")
         return None, None
 
     api_url = f"{repo_url}/releases"
-    response = requests.get(api_url)
+    response = await AsyncClient().get(api_url)
     if response.status_code == 200:
         releases = response.json()
         if prerelease:
             for release in releases:
                 if release["prerelease"]:
-                    version, asset_url = get_version_url(release)
+                    version, asset_url = await get_version_url(release)
                     return version, asset_url
         else:
             for release in releases:
-                if release["prerelease"] is False:
-                    version, asset_url = get_version_url(release)
+                if not release["prerelease"]:
+                    version, asset_url = await get_version_url(release)
                     return version, asset_url
+    print(f"No release found for the {repo_url}")
     return None, None
 
 
-def main():
+async def main():
     with open('sources.json') as file:
         sources = json.load(file)
 
-    for source in sources:
-        patches_version, patches_asset_url = get_latest_release(sources.get(source).get('patches'), 
-                               sources.get(source).get('prerelease', False))
-        integration_version, integration_asset_url = get_latest_release(sources.get(source).get('integration'),
-                                   sources.get(source).get('prerelease', False))
+    for source, repo in sources.items():
+        patches_version, patches_asset_url = await get_latest_release(repo.get('patches'),
+                    repo.get('prerelease', False))
+        integration_version, integration_asset_url = await get_latest_release(repo.get('integration'),
+                    repo.get('prerelease', False))
         info_dict = {
             "patches": {
                 "version": patches_version,
@@ -52,4 +55,4 @@ def main():
         print(f"Latest release information saved to {source}-bundle.json")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
